f09b8b896d16572c271cfb7dd46448484132e4ff
Kolja Sam <github@koljapluemer.com>
2025-12-14 11:41:47 +0100

    fix slug gen

--- app/main-process/storage/slug.ts
+++ app/main-process/storage/slug.ts
@@ -6,9 +6,10 @@ export function deriveSlug(text: string): string {
     throw new Error('Content must produce a valid slug.')
   }
 
+  // ONLY remove actually illegal characters and trim to reasonable length
+  // do NOT remove punctuation, spaces or anything else
   let slug = text.replace(/[/\\?*:|"<>]/g, '')
   slug = slug.replace(/[\x00-\x1F]/g, '')
-  slug = slug.replace(/[\s.]+$/g, '')
 
   if (slug.length > 120) {
     slug = slug.substring(0, 120).replace(/[\s.]+$/g, '')
